746867f68b587d463904c294e9ef62bcf7025fbd
huangjiaqi <huangjiaqi@pinquest.cn>
2020-11-12 18:22:13 +0800

    chore: update

--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,18 @@ gulp.task('css', () => {
   return gulp
     .src('src/css/index.css')
     .pipe(cssFormat({ indent: 1, hasSpace: true }))
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('lib/'));
 });
 
 gulp.task('image', () => {
-  return gulp.src('src/img/*').pipe(gulp.dest('dist/img/'));
+  return gulp.src('src/img/*').pipe(gulp.dest('lib/img/'));
 });
 
 gulp.task('js', () => {
-  return gulp.src('src/emojis.wx.js').pipe(rename('index.js')).pipe(gulp.dest('dist/'));
+  return gulp.src('src/emojis.wx.js').pipe(rename('index.js')).pipe(gulp.dest('lib/'));
 });
 
-gulp.task('clean', () => del(['dist/*'], { dot: true }));
+gulp.task('clean', () => del(['lib/*'], { dot: true }));
 
 gulp.task(
   'default',
